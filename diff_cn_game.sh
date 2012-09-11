776327c9d16e7b8f4e9fb3ee5662442b7b8fdbed
yuzebin <yuzebin@gmail.com>
2012-09-11 18:01:54 +0800

    purge files, delete the files noused.

--- diff_cn_game.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/diff -q `ls -t /root/rank/cn_game_all*.txt | head -n 2` >>/root/cn_game_all_chg.txt
-/usr/bin/diff -q `ls -t /root/rank/cn_all*.txt | head -n 2` >>/root/cn_all_chg.txt
-/usr/bin/diff -q `ls -t /root/rank/cn_wheather*.txt | head -n 2` >>/root/cn_wheather_chg.txt
--- view_rank_change.sh
+++
@@ -1,8 +0,0 @@
-echo 总榜 
-tail -n 1 cn_all_chg.txt | sed -e "s:Files:\ \ \ :g" -e "s:\ and\ :\n    :g"
-
-echo 游戏
-tail -n 1 cn_game_all_chg.txt | sed -e "s:Files:\ \ \ :g" -e "s:\ and\ :\n    :g"
-
-echo 天气
-tail -n 1 cn_wheather_chg.txt | sed -e "s:Files:\ \ \ :g" -e "s:\ and\ :\n    :g"
